ffff22ac671b7e100837d8dbefb2edc2a3cccbcc
Lukas Zapletal <lzap+git@redhat.com>
2012-08-03 20:33:21 +0200

    release script

--- release.sh
+++ release.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-ROOT=~/rpmbuild
-mkdir -p $ROOT/SOURCES 2>/dev/null
-waf distclean dist
-mv fourterm-*.tar.bz2 $ROOT/SOURCES
-rpmbuild -ba fourterm.spec --clean --rmsource
-mkdir -p dist 2>/dev/null
-mv $ROOT/SRPMS/fourterm*rpm $ROOT/RPMS/x86_64/fourterm*rpm dist
-#rm -rf $ROOT
+DIR=~/work/fedorapeople/public_html/projects/fourterm/releases
+tito build --srpm -o $DIR
+LATESTSRPM=$(ls -ct1 $DIR/*src.rpm | head -1)
+koji build --scratch --nowait dist-rawhide $LATESTSRPM
+$DIR/../../../../copy-content
